924dfb7d8f8b18c0c31ccc162878d41f115cf337
Shiva Kumar Vasetti <shivakumarvasetti@gmail.com>
2026-03-02 21:45:55 +0530

    UART.v

--- Complete_UART.v
+++ Complete_UART.v
@@ -1,36 +1,21 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.08.2025 11:28:45
-// Design Name: 
-// Module Name: Complete_UART
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module UART(CLK,RST,Tx_done,Data_in,Data_out,Tx_count,flag_sig,TX_States,RX_States,RX_Data,Rx_count);
+input CLK;
+input RST;
+input [7:0]Data_in;
+output [4:0]Tx_count;
+output [4:0]Rx_count;
+output wire Tx_done;
+output Data_out;
+output  [7:0]RX_Data;
+////test signals//////////////
+output flag_sig;
+output [1:0]TX_States;
+output [1:0]RX_States;
+/////////////////////////////
 
 
-module Complete_UART(TX_start,UART_out,UART_in,main_clk,main_reset,RX_done,tx_done,flag_out);
-output [7:0]UART_out;
-output RX_done;
-output tx_done,flag_out;
-input [7:0]UART_in;
-input main_clk,main_reset,TX_start;
-wire [7:0] data_bridge;
-
-baud_rate_gen BAUD_RATE_GENERATOR(.flag(flag_out),.clk(main_clk),.rst(main_reset));
-UART_Transmitter_ony Transmitter(.tx_done(tx_done),.tx_start(TX_start),.clock(main_clk),.d_in(UART_in),.reset(main_reset),.d_out(data_bridge),.flag(flag_out));
-UART_Rx Receiver(.rx_start(tx_done),.rx_flag(flag_out),.bit_in(data_bridge),.Rx_output(UART_out),.rx_done(RX_done),.R_reset(main_reset),.R_clk(main_clk));
-
-
+Baud_rate_gen BAUD_RATE_GENERATOR(.flag(flag_sig),.clk(CLK),.rst(RST));
+UART_TX UART_TRA(.t_done(Tx_done),.data_out(Data_out),.data(Data_in),.clk(CLK),.rst(RST),.tx_count(Tx_count),.flag_in(flag_sig),.ps(TX_States));
+UART_RX UART_REC(.r_done(r_done),.rx_data(RX_Data),.rx_data_in(Data_out),.clk(CLK),.rst(RST),.rx_count(Rx_count),.flag_in(flag_sig),.ps(RX_States));
 endmodule
